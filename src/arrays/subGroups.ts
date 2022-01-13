631fa869c0966b0486fe54e17c4b04e341fb5ac6
shahar shavit <shahars2@rnd-hub.com>
2022-01-13 08:56:56 +0200

    chore: added jsDoc to sun groups

--- src/arrays/subGroups.ts
+++ src/arrays/subGroups.ts
@@ -1,21 +1,27 @@
-function* subGroupsGen<T>(layers: T[], maxLength: number, includeOnlyMaxLength = false): Generator<T[]> {
+/**
+ * split group into unique sub groups ordered by size from largest to smallest
+ * @param group input group
+ * @param maxLength length of the largest sub group return. must be <= group.length
+ * @param includeOnlyMaxLength indicates if function will return only sub groups in the specified "maxLength" or smaller groups as well. default: false
+ */
+function* subGroupsGen<T>(group: T[], maxLength: number, includeOnlyMaxLength = false): Generator<T[]> {
   if (maxLength === 0) {
     yield [];
   } else {
-    if (layers.length === maxLength) {
-      yield layers;
+    if (group.length === maxLength) {
+      yield group;
     } else {
-      const lengthDif = layers.length - maxLength;
+      const lengthDif = group.length - maxLength;
       for (let i = 0; i <= lengthDif; i++) {
-        const subLayers = layers.slice(i + 1);
+        const subLayers = group.slice(i + 1);
         const subPermutations = subGroupsGen(subLayers, maxLength - 1);
         for (const subPermutation of subPermutations) {
-          yield [layers[i], ...subPermutation];
+          yield [group[i], ...subPermutation];
         }
       }
     }
     if (maxLength > 1 && !includeOnlyMaxLength) {
-      yield* subGroupsGen(layers, maxLength - 1);
+      yield* subGroupsGen(group, maxLength - 1);
     }
   }
 }
