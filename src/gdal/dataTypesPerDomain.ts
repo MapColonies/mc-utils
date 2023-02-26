0fd03160e71f0f76a2b37b98a7143cf61b143e7f
assafchamoy <assafchamoy97@gmail.com>
2023-02-26 11:08:36 +0200

    fix: refactor gdal data types

--- src/gdal/dataTypesPerDomain.ts
+++ src/gdal/dataTypesPerDomain.ts
@@ -1,20 +1,14 @@
 import { RecordType } from '@map-colonies/mc-model-types';
 import { DataType } from '../models/enums/gdal';
 
-export const ALL_DATA_TYPES = Object.values(DataType);
-export const DEM_DATA_TYPES = [DataType.INT16, DataType.INT32, DataType.INT64, DataType.FLOAT32, DataType.FLOAT64];
+export const DATA_TYPES_MAP: Map<Partial<RecordType>, DataType[]> = new Map([
+  [RecordType.RECORD_DEM, [DataType.INT16, DataType.INT32, DataType.INT64, DataType.FLOAT32, DataType.FLOAT64]],
+  [RecordType.RECORD_ALL, Object.values(DataType)],
+]);
 
-export const getDataTypesNoInterpolation = (currentType: DataType, domain?: RecordType): DataType[] => {
-  let validDataTypeForDomain: DataType[];
-  switch (domain) {
-    case RecordType.RECORD_DEM:
-      validDataTypeForDomain = DEM_DATA_TYPES;
-      break;
-    default:
-      validDataTypeForDomain = ALL_DATA_TYPES;
-      break;
-  }
+export const getDataTypesNoInterpolation = (currentType: DataType, domain: RecordType = RecordType.RECORD_ALL): DataType[] => {
+  const dataTypeForDomain = DATA_TYPES_MAP.get(domain) as DataType[];
 
-  const currentTypeIdx = validDataTypeForDomain.findIndex((type) => type === currentType);
-  return validDataTypeForDomain.slice(0, currentTypeIdx + 1);
+  const currentTypeIdx = dataTypeForDomain.findIndex((type) => type === currentType);
+  return dataTypeForDomain.slice(0, currentTypeIdx + 1);
 };
