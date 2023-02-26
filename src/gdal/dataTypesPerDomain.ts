775a59fb5cf794402c7262738bdcea091fc5e051
assafchamoy <assafchamoy97@gmail.com>
2023-02-26 11:42:34 +0200

    fix: add js doc to the helper function

--- src/gdal/dataTypesPerDomain.ts
+++ src/gdal/dataTypesPerDomain.ts
@@ -6,6 +6,12 @@ export const DATA_TYPES_MAP: Map<Partial<RecordType>, DataType[]> = new Map([
   [RecordType.RECORD_ALL, Object.values(DataType)],
 ]);
 
+/**
+ * Returns a list of data types lower in value than the provided type in order to prevent interpolations for gdal operations.
+ * @param {DataType} currentType The current data type to build the list upon.
+ * @param {RecordType} [domain = RecordType.RECORD_ALL] domain The requested domain for the original valid data types list.
+ * @returns DataType[]
+ */
 export const getDataTypesNoInterpolation = (currentType: DataType, domain: RecordType = RecordType.RECORD_ALL): DataType[] => {
   const dataTypeForDomain = DATA_TYPES_MAP.get(domain) as DataType[];
 
