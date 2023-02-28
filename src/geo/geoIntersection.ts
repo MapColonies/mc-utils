9e03f46dc89c7b3220427891ac4d005eeeda1905
ronenk1 <ronenk1@rnd-hub.com>
2023-02-28 07:43:35 +0200

    chore: adding featureCollection booleanEqual util

--- src/geo/geoIntersection.ts
+++ src/geo/geoIntersection.ts
@@ -1,9 +1,42 @@
-import { Feature, intersect, MultiPolygon, Polygon } from '@turf/turf';
+import { Feature, FeatureCollection, intersect, MultiPolygon, Polygon } from '@turf/turf';
+import _ from 'lodash';
 
 /**
  * tuft intersect supported footprint types
  */
 declare type Footprint = Polygon | MultiPolygon | Feature<Polygon | MultiPolygon>;
+
+const featuresCustomizer = (objectValue: Feature[], otherValue: Feature[]): boolean => {
+  // compare features
+  // https://lodash.com/docs/4.17.15#find
+
+  if (!_.isEqual(objectValue.length, otherValue.length)) {
+    return false;
+  }
+  for (let i = 0; i < objectValue.length; i++) {
+    const isFeatureContained = _.find(otherValue, objectValue[i]);
+    if (isFeatureContained === undefined) {
+      return false;
+    }
+  }
+  for (let i = 0; i < otherValue.length; i++) {
+    const isFeatureContained = _.find(objectValue, otherValue[i]);
+    if (isFeatureContained === undefined) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const featureCollectionCustomized = (objectValue: FeatureCollection, otherValue: FeatureCollection): boolean => {
+  // compare type
+  if (!_.isEqual(objectValue.type, otherValue.type)) {
+    return false;
+  }
+  // compare features
+  const isFeaturesEqual = _.isEqualWith(objectValue.features, otherValue.features, featuresCustomizer);
+  return isFeaturesEqual;
+};
 
 /**
  * return the intersection footprint of all specified footprints or null when there is no intersection
@@ -27,4 +60,14 @@ const multiIntersect = (footprints: Footprint[]): Footprint | null => {
   return intersection;
 };
 
-export { multiIntersect, Footprint };
+/**
+ * indicates if 2 object of type FeatureCollection are equal by validating properties and features array (order not important)
+ * @param fc1 first featureCollection to compare equal
+ * @param fc2 second featureCollection to compare equal
+ * @returns true if same featureCollection, false if not
+ */
+const roiBooleanEqual = (fc1: FeatureCollection, fc2: FeatureCollection): boolean => {
+  return _.isEqualWith(fc1, fc2, featureCollectionCustomized);
+};
+
+export { multiIntersect, roiBooleanEqual, Footprint };
--- tests/unit/geo/geoIntersection.spec.ts
+++ tests/unit/geo/geoIntersection.spec.ts
@@ -1,5 +1,6 @@
-import { bboxPolygon } from '@turf/turf';
-import { multiIntersect } from '../../../src';
+import { bboxPolygon, FeatureCollection } from '@turf/turf';
+import { multiIntersect, roiBooleanEqual } from '../../../src';
+import { fc1, fc2, fcNoProperties } from '../data/testData';
 
 describe('geoIntersections', () => {
   describe('multiIntersect', () => {
@@ -19,6 +20,38 @@ describe('geoIntersections', () => {
       const expected = bboxPolygon([1, 1, 2, 2]);
       expected.bbox = undefined;
       expect(intersection).toEqual(expected);
+    });
+  });
+
+  describe('roiBooleanEqual', () => {
+    it('returns true to comparing two featureCollection with different feature order', () => {
+      const isEqual = roiBooleanEqual(fc1, fc2);
+      expect(isEqual).toBeTruthy();
+    });
+
+    it('returns false to comparing two featureCollection with not same features', () => {
+      const fc3: FeatureCollection = { ...fc1, features: fc1.features.slice(1) };
+      const isEqual = roiBooleanEqual(fc1, fc3);
+      expect(isEqual).toBeFalsy();
+    });
+
+    it('returns false to comparing two featureCollection with not same properties', () => {
+      const isEqual = roiBooleanEqual(fc1, fcNoProperties);
+      expect(isEqual).toBeFalsy();
+    });
+
+    it('returns true to comparing two nullable', () => {
+      const isEqual = roiBooleanEqual(
+        {
+          type: 'FeatureCollection',
+          features: [],
+        },
+        {
+          type: 'FeatureCollection',
+          features: [],
+        }
+      );
+      expect(isEqual).toBeTruthy();
     });
   });
 });
