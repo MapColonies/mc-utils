a5a9376484e2fd5df8e2052f958f133022f4baeb
ronenkapelian <72082238+ronenkapelian@users.noreply.github.com>
2023-02-28 09:47:58 +0200

    chore: adding featureCollection booleanEqual util (#21)
    * chore: adding featureCollection booleanEqual util
    
    * fix: pr notes
    
    * fix: test doc

--- tests/configurations/unit/jest.config.js
+++ tests/configurations/unit/jest.config.js
@@ -20,10 +20,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 66,
+      functions: 94,
+      lines: 89,
+      statements: 90,
     },
   },
 };
