55059b0ce13836590d0292a7bdbb1ecf3d675e42
Ronit Kissis <92728393+RonitKissis@users.noreply.github.com>
2024-04-07 11:50:20 +0300

    feat: add promiseTimeout function (#27)
    * feat: add promiseTimeout function
    
    * fix: function coverage
    
    * fix: pr changes
    
    * fix: functions coverage

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './communication';
 export * from './dateTime';
 export * from './geo';
 export * from './arrays';
+export * from './utils';
--- src/utils/timeout.ts
+++ src/utils/timeout.ts
@@ -1,3 +1,18 @@
 export async function timeout(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export class TimeoutError extends Error {}
+
+export const racePromiseWithTimeout = async <T>(msTimeout: number, promise: Promise<T>): Promise<T> => {
+  // Create a promise that rejects in <ms> milliseconds
+  const timeout = new Promise<T>((_, reject) => {
+    const id = setTimeout(() => {
+      clearTimeout(id);
+      reject(new TimeoutError(`Timed out in + ${msTimeout} + ms.`));
+    }, msTimeout);
+  });
+
+  // Returns a race between our timeout and the passed in promise
+  return Promise.race([promise, timeout]);
+};
