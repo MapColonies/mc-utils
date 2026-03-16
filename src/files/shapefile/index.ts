ecbd2768acc94aab6370ce257af1cd446e6770be
almog8k <almog8k@gmail.com>
2026-03-16 11:33:46 +0200

    refactor!: remove ShapefileReader, moved to external package

--- src/files/shapefile/index.ts
+++
@@ -1,3 +0,0 @@
-export { ShapefileChunkReader } from './core/shapeFileReader';
-export { openShapefile, GdalShapefileReader, IShapefileSource, ShapefileReadResult } from './core/gdalShapefileReader';
-export * from './types';
